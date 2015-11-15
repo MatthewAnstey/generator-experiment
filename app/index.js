ca8bed304e101ab3d6f0665fa1bb3d96c86cb6ac
= <mtthwnsty@gmail.com>
2015-11-15 22:47:08 +0000

    Initial Bootstrap commit

---
+++ app/index.js
@@ -0,0 +1,59 @@
+var generators = require('yeoman-generator');
+var path = require('path');
+var editor = require('mem-fs-editor');
+var mkdirp = require('mkdirp');
+
+module.exports = generators.Base.extend({
+    constructor: function() {
+    	this.realCWD = process.cwd();
+
+    	generators.Base.apply(this, arguments);
+
+        this.argument('file', { type: String, required: true });
+
+        this.subTestFolders = ['Spec', 'Setup', 'Config'];
+
+        this.GLOBAL_SPEC = 'tests/jasmine_tests/spec';
+    },
+    wiring: function() {
+        mkdirp(this.GLOBAL_SPEC + this._getFilePath(), function (err) {
+            if (err) console.error(err)
+        });
+
+        for (var i = 0; i < this.subTestFolders.length; i++) {
+        	this._writeTestFiles(this.subTestFolders[i]);
+        }        
+    },
+    _getFilePath: function() {
+    	var locationOfFile = this.realCWD;
+    	var skinPath = locationOfFile.slice(locationOfFile.indexOf('/skin/'));
+
+    	return skinPath + '/' + this._getTestFolder();
+    },
+    _getFileArray: function (path) {
+    	var pathArray = path.split('/');
+
+    	pathArray.shift();
+
+    	return pathArray;
+    },
+    _getFileWithoutExt: function () {
+    	var fileLength = this.file.length;
+    	return this.file.substring(0, fileLength - 3);
+    },
+    _getTestFolder: function () {
+        var folderStart = this._getFileWithoutExt();
+        var folderEnd = 'Test';
+
+    	return folderStart + folderEnd;
+    },
+    _writeTestFiles: function (testFolderEndName){
+    	var testPath = this.GLOBAL_SPEC + 
+    	    this._getFilePath() + 
+    	    '/' + 
+    	    this._getFileWithoutExt() +
+    	    testFolderEndName;
+
+    	this.fs.write(testPath, 'Write a test');
+    }
+});
