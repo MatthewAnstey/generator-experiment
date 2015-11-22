ea9a27ec817bc77a49a0a58ba79ab4b28cfb291e
= <mtthwnsty@gmail.com>
2015-11-22 22:54:51 +0000

    Add prompt to create bespoke config file

--- app/index.js
+++ app/index.js
@@ -4,53 +4,73 @@ var mkdirp = require('mkdirp');
 
 module.exports = generators.Base.extend({
     constructor: function() {
-    	this.realCWD = process.cwd();
+        this.realCWD = process.cwd();
 
-    	generators.Base.apply(this, arguments);
+        generators.Base.apply(this, arguments);
 
-        this.argument('file', { type: String, required: true });
+        this.argument('file', {
+            type: String,
+            required: true
+        });
 
         this.subTestFolders = ['Spec', 'Setup', 'Config'];
 
         this.GLOBAL_SPEC = 'tests/jasmine_tests/spec';
     },
     wiring: function() {
-        mkdirp(this.GLOBAL_SPEC + this._getFilePath(), function (err) {
+
+        mkdirp(this.GLOBAL_SPEC + this._getFilePath(), function(err) {
             if (err) console.error(err)
         });
 
-        for (var i = 0; i < this.subTestFolders.length; i++) {
-        	this._writeTestFiles(this.subTestFolders[i]);
+        if (this.config.get('bespokeConfig')) {
+            this.subTestFolders.pop();
         }
-        
+
+        for (var i = 0; i < this.subTestFolders.length; i++) {
+            this._writeTestFiles(this.subTestFolders[i]);
+        }
+
+    },
+    prompting: function() {
+        var done = this.async();
+        this.prompt({
+            type: 'confirm',
+            name: 'bespokeConfig',
+            message: 'Would you like to create a config file?',
+        }, function(answer) {
+            this.config.set(answer);
+            done();
+        }.bind(this));
     },
     _getFilePath: function() {
-    	var locationOfFile = this.realCWD;
-    	var skinPath = locationOfFile.slice(locationOfFile.indexOf('/skin/'));
+        var locationOfFile = this.realCWD;
+        var skinPath = locationOfFile.slice(locationOfFile.indexOf('/skin/'));
 
-    	return skinPath + '/' + this._getTestFolder();
+        return skinPath + '/' + this._getTestFolder();
     },
-    _getFileWithoutExt: function () {
-    	var fileLength = this.file.length;
-    	return this.file.substring(0, fileLength - 3);
+    _getFileWithoutExt: function() {
+        var fileLength = this.file.length;
+        return this.file.substring(0, fileLength - 3);
     },
-    _getTestFolder: function () {
+    _getTestFolder: function() {
         var folderStart = this._getFileWithoutExt();
         var folderEnd = 'Test';
 
-    	return folderStart + folderEnd;
+        return folderStart + folderEnd;
     },
-    _writeTestFiles: function (testFolderEndName){
-    	var testPath = this.GLOBAL_SPEC + 
-    	    this._getFilePath() + 
-    	    '/' + 
-    	    this._getFileWithoutExt() +
-    	    testFolderEndName;
+    _writeTestFiles: function(testFolderEndName) {
+        var testPath = this.GLOBAL_SPEC +
+            this._getFilePath() +
+            '/' +
+            this._getFileWithoutExt() +
+            testFolderEndName;
 
-    	this.fs.copyTpl(
-    	    this.templatePath(testFolderEndName + '.js'),
-    	    this.destinationPath(testPath  + '.js'),
-    	    { filename: this._getFileWithoutExt() }
-    	);
+        this.fs.copyTpl(
+            this.templatePath(testFolderEndName + '.js'),
+            this.destinationPath(testPath + '.js'), {
+                filename: this._getFileWithoutExt()
+            }
+        );
     }
 });
